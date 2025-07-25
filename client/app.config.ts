8fde7d334b135a2c52bd7c84cab03d40b10af5fd
CutyDog <silver.soul.818@gmail.com>
2025-07-25 22:06:20 +0900

    test

--- client/app.config.ts
+++ client/app.config.ts
@@ -1,58 +1,59 @@
-export default {
-  expo: {
-    name: "fitness-app",
-    slug: "fitness-app",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "client",
-    userInterfaceStyle: "automatic",
-    newArchEnabled: true,
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: "com.nnf3.fitness-app",
-      googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST ?? "./GoogleService-Info.plist",
-      infoPlist: {
-        ITSAppUsesNonExemptEncryption: false
-      }
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "fitness-app",
+  slug: "fitness-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon-muscle.png",
+  scheme: "client",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.nnf3.fitness-app",
+    googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST ?? "./GoogleService-Info.plist",
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false
+    }
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
-      edgeToEdgeEnabled: true,
-      package: "com.nnf3.fitnessapp",
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json"
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png"
-    },
-    plugins: [
-      "@react-native-firebase/app",
-      "@react-native-firebase/auth",
-      "@react-native-firebase/crashlytics",
-      "@react-native-google-signin/google-signin",
-      [
-        "expo-build-properties",
-        {
-          "ios": {
-            "useFrameworks": "static"
-          }
+    edgeToEdgeEnabled: true,
+    package: "com.nnf3.fitnessapp",
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json"
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "@react-native-firebase/app",
+    "@react-native-firebase/auth",
+    "@react-native-firebase/crashlytics",
+    "@react-native-google-signin/google-signin",
+    [
+      "expo-build-properties",
+      {
+        "ios": {
+          "useFrameworks": "static"
         }
-      ],
-      "expo-router"
-    ],
-    experiments: {
-      typedRoutes: true
-    },
-    extra: {
-      router: {},
-      eas: {
-        projectId: "21d4c7ec-1763-4f0d-b0bd-708adbe40897"
       }
+    ],
+    "expo-router"
+  ],
+  experiments: {
+    typedRoutes: true
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "21d4c7ec-1763-4f0d-b0bd-708adbe40897"
     }
   }
-}
+})
