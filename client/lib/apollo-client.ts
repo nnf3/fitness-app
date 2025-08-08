ac06e73ac46ff52b743195e25bfe0237a80eb912
CutyDog <silver.soul.818@gmail.com>
2025-08-09 04:49:48 +0900

    remove EXPO_PUBLIC_SERVER_PORT

--- client/lib/apollo-client.ts
+++ client/lib/apollo-client.ts
@@ -10,17 +10,13 @@ const getGraphQLUri = () => {
 
   // 環境変数からサーバー情報を取得
   const serverIP = process.env.EXPO_PUBLIC_SERVER_IP || 'localhost';
-  const serverPort = process.env.EXPO_PUBLIC_SERVER_PORT || '8080';
 
-  if (Platform.OS === 'ios') {
-    // iOSシミュレータ用: ホストマシンのIPアドレスを使用
-    return `http://${serverIP}:${serverPort}/query`;
-  } else if (Platform.OS === 'android') {
-    // Androidエミュレータ用: 10.0.2.2を使用
-    return `http://10.0.2.2:${serverPort}/query`;
+  if (Platform.OS === 'ios' || Platform.OS === 'android') {
+    // iOS/Android用: ホストマシンのIPアドレスを使用
+    return `http://${serverIP}:8080/query`;
   } else {
     // Web用: localhostを使用
-    return `http://localhost:${serverPort}/query`;
+    return `http://localhost:8080/query`;
   }
 };
 
