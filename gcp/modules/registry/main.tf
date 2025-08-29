9b7e32b5f987a2d309a1975a7f9bb05508b22d0a
CutyDog <silver.soul.818@gmail.com>
2025-08-29 22:57:03 +0900

    add cleanup_policies

--- gcp/modules/registry/main.tf
+++ gcp/modules/registry/main.tf
@@ -4,6 +4,30 @@ resource "google_artifact_registry_repository" "api" {
   repository_id = "fitness-api"
   description   = "Fitness API"
   format        = "DOCKER"
+
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+    condition {
+      tag_state = "UNTAGGED"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-new-untagged"
+    action = "KEEP"
+    condition {
+      tag_state  = "UNTAGGED"
+      newer_than = "7d"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 3
+    }
+  }
 }
 
 resource "google_artifact_registry_repository" "migrate" {
@@ -12,6 +36,30 @@ resource "google_artifact_registry_repository" "migrate" {
   repository_id = "fitness-api-migrate"
   description   = "Fitness API Migrate"
   format        = "DOCKER"
+
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+    condition {
+      tag_state = "UNTAGGED"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-new-untagged"
+    action = "KEEP"
+    condition {
+      tag_state  = "UNTAGGED"
+      newer_than = "7d"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 3
+    }
+  }
 }
 
 resource "google_artifact_registry_repository" "seed" {
@@ -20,4 +68,28 @@ resource "google_artifact_registry_repository" "seed" {
   repository_id = "fitness-api-seed"
   description   = "Fitness API Seed"
   format        = "DOCKER"
+
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+    condition {
+      tag_state = "UNTAGGED"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-new-untagged"
+    action = "KEEP"
+    condition {
+      tag_state  = "UNTAGGED"
+      newer_than = "7d"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      keep_count = 3
+    }
+  }
 }
